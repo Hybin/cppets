0b8eb99b2ad29e9839927b2e63212df029c26603
Hybin <hhb8550@live.com>
2021-01-12 00:08:21 +0800

    feat: 新增 UnionFind 并查集算法 & 格式调整

--- include/cppets.hpp
+++ include/cppets.hpp
@@ -5,26 +5,22 @@
 #include <string>
 #include <cmath>
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    // constructor
-    ListNode(int x) : val(x), next(NULL) {};
-};
-
 namespace cppets {
     // split the string with specific delimiter
     std::vector<std::string> split(const std::string &str, const std::string &delimiter);
     
     // check if the number is a prime
     bool isPrime(int &n);
-
-    // reverse the ListNode
-    ListNode* reverseListNode(ListNode *head);
     
     // trim the string
     std::string trim(std::string &str);
+
+    struct ListNode;
+
+    // reverse the ListNode
+    ListNode* reverseListNode(ListNode *head);
+
+    class UnionFind;
 }
 
 #endif
--- src/cppets.cpp
+++ src/cppets.cpp
@@ -2,62 +2,123 @@
 
 using namespace std;
 
-namespace cppets {
-    vector<string> split(const string &str, const string &delimiter) {
-        vector<string> result;
-        string::size_type start = 0, end = str.find(delimiter);
+struct cppets::ListNode {
+    int val;
+    ListNode *next;
 
-        while (end != string::npos) {
-            result.push_back(str.substr(start, end - start));
+    // constructor
+    ListNode(int x) : val(x), next(NULL) {};
+};
 
-            start = end + delimiter.size();
-            end = str.find(delimiter, start);
+class cppets::UnionFind {
+public:
+    UnionFind(int n) {
+        this->count = n;
+        this->parent = new int[n];
+        this->size = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            this->parent[i] = i;
+            this->size[i] = 1;
         }
-
-        if (start != str.length()) {
-            result.push_back(str.substr(start));
-        }
-
-        return result;
     }
 
-    bool isPrime(int &n) {
-        if (n <= 3) return n > 1;
-        
-        if (n % 6 != 1 && n % 6 != 5) return false;
-        
-        int s = sqrt(n);
-        for (int i = 5; i <=s; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
+    void connect(int p, int q) {
+        int rootP = this->find(p);
+        int rootQ = this->find(q);
+        if (rootP == rootQ) {
+            return;
         }
         
-        return true;
+        // 小树接到大树下面，较平衡
+        if (this->size[rootP] > this->size[rootQ]) {
+            this->parent[rootQ] = rootP;
+            this->size[rootP] += this->size[rootQ];
+        } else {
+            this->parent[rootP] = rootQ;
+            this->size[rootQ] += this->size[rootP];
+        }
+        this->count--;
     }
 
-    ListNode* reverseListNode(ListNode *head) {
-        ListNode *prev = NULL, *curr = head;
-
-        while (curr != NULL) {
-            ListNode *next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+    int find(int x) {
+        while (this->parent[x] != x) {
+            // 进行路径压缩
+            this->parent[x] = this->parent[this->parent[x]];
+            x = this->parent[x];
         }
-
-        return prev;
+        return x;
     }
 
-    std::string trim(std::string &str) {
-        if (str.empty()) {
-            return str;
+    bool connected(int p, int q) {
+        int rootP = this->find(p);
+        int rootQ = this->find(q);
+        return rootP == rootQ;
+    }
+
+    int getCount() {
+        return this->count;
+    }
+private:
+    int count;
+    int *parent;
+    int *size;
+};
+
+vector<string> cppets::split(const string &str, const string &delimiter) {
+    vector<string> result;
+    string::size_type start = 0, end = str.find(delimiter);
+
+    while (end != string::npos) {
+        result.push_back(str.substr(start, end - start));
+
+        start = end + delimiter.size();
+        end = str.find(delimiter, start);
+    }
+
+    if (start != str.length()) {
+        result.push_back(str.substr(start));
+    }
+
+    return result;
+}
+
+bool cppets::isPrime(int &n) {
+    if (n <= 3) return n > 1;
+        
+    if (n % 6 != 1 && n % 6 != 5) return false;
+        
+    int s = sqrt(n);
+    for (int i = 5; i <=s; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
         }
-    
-        const std::string space = " ";
-        str.erase(0, str.find_first_not_of(space));
-        str.erase(str.find_last_not_of(space) + 1);
-    
+    }
+        
+    return true;
+}
+
+std::string cppets::trim(std::string &str) {
+    if (str.empty()) {
         return str;
     }
+    
+    const std::string space = " ";
+    str.erase(0, str.find_first_not_of(space));
+    str.erase(str.find_last_not_of(space) + 1);
+    
+    return str;
+}
+
+cppets::ListNode* cppets::reverseListNode(cppets::ListNode *head) {
+    cppets::ListNode *prev = NULL, *curr = head;
+
+    while (curr != NULL) {
+        cppets::ListNode *next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
 }
